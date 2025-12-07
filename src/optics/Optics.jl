cd65b455cd49facd22067418c97aa47e14d01849
chrsdavis <84039785+chrsdavis@users.noreply.github.com>
2025-12-07 17:37:46 -0500

    composition of optics

--- src/optics/Optics.jl
+++ src/optics/Optics.jl
@@ -18,7 +18,9 @@ end
 # TODO: restrict from ANY using AJ morphism type(s)
 
 
-# TODO: left_unitor and right_unitor
+# TODO: left_unitor() and right_unitor()
+# TODO: inv()
+# TODO: associator()
 
 """
     id_optic(C, S, T)
@@ -37,5 +39,40 @@ function id_optic(C, S, T)
 
     return Optic{typeof(C),S,S,T,T,typeof(I)}(C, forward, backward)
 end
+
+
+"""
+    compose_optic(o2, o1)
+
+Compose optics (o1 : (S,T)->(A,B)) and (o2 : (A,B)->(U,V))
+to get an optic (S,T)->(U,V).
+"""
+function compose_optic(o2::Optic, o1::Optic)
+    C = o1.C
+    @assert C === o2.C  "Base categories must match" # TODO: does this have to hold...?
+
+    # Aliases to make things moe clear
+    l1, r1 = o1.forward, o1.backward
+    l2, r2 = o2.forward, o2.backward
+    M1, M2 = o1.M, o2.M  # Could store M as field or recompute from morph domains
+
+    # Build residual / structural isos
+    M  = tensor(C, M1, M2)  # M = M1 ⊗ M2
+
+    # forward: S → M ⊗ U
+    f1 = l1                          # S → M1 ⊗ A
+    f2 = tensor(C, id(C,M1), l2)     # M1 ⊗ A → M1 ⊗ (M2 ⊗ U)
+    α  = associator(C, M1, M2, o2.A) # (M1 ⊗ (M2 ⊗ U)) → (M1 ⊗ M2) ⊗ U
+    forward  = α ∘ f2 ∘ f1
+
+    # backward: M ⊗ V → T
+    α⁻¹      = inv(associator(C, M1, M2, o2.B))
+    b1       = α⁻¹                            # (M1 ⊗ M2) ⊗ V → M1 ⊗ (M2 ⊗ V)
+    b2       = tensor(C, id(C,M1), r2)        # M1 ⊗ (M2 ⊗ V) → M1 ⊗ B
+    backward = r1 ∘ b2 ∘ b1
+
+    return Optic{typeof(C),o1.S,o1.A,o2.T,o2.B,typeof(M)}(C, forward, backward)
+end
+
 
 end # module Optics
